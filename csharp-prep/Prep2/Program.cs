ab7dcd02206a8657af0853d7be74363a08add338
Jonathan Gaibor <jonathan.gaibor.63@gmail.com>
2023-09-19 22:24:58 -0500

    finish prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        double porcentajeCalificacion = Convert.ToDouble(Console.ReadLine());
+
+        char calificacion;
+
+        if (porcentajeCalificacion >= 90)
+        {
+            calificacion = 'A';
+        }
+        else if (porcentajeCalificacion >= 80)
+        {
+            calificacion = 'B';
+        }
+        else if (porcentajeCalificacion >= 70)
+        {
+            calificacion = 'C';
+        }
+        else if (porcentajeCalificacion >= 60)
+        {
+            calificacion = 'D';
+        }
+        else
+        {
+            calificacion = 'F';
+        }
+
+        Console.WriteLine("your grade is: " + calificacion);
+
+        if (porcentajeCalificacion >= 70)
+        {
+            Console.WriteLine("¡Felicidades! Has aprobado el curso.");
+        }
+        else
+        {
+            Console.WriteLine("Ánimo, puedes intentarlo de nuevo la próxima vez.");
+        }
     }
 }
